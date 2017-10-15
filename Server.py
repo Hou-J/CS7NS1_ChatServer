2e84f395b3c5fef1d7a11196188910b8f244eb76
jhou <jiongxuhou@gmail.com>
2017-10-15 15:52:12 +0000

    init

---
+++ Server.py
@@ -0,0 +1,14 @@
+import sys, socket
+
+if (len(sys.argv) < 3):
+    print("Server usage: python server.py [IP] [PORT]")
+    sys.exit(0)
+
+class Server:
+    pass
+
+class Chatroom:
+    pass
+
+class Client:
+    pass
